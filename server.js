ea01682b7c51c59b72c25f6df07767528d1e8c24
GrazianoLapa <grazianoempresarial.ia@gmail.com>
2025-12-15 05:46:48 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import pkg from "pg";
+import cors from "cors";
+
+const { Pool } = pkg;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      status: "Backend online",
+      database_time: result.rows[0]
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta", PORT);
+});
